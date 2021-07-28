c62a6f07426feabb4059ba029aceaed448d626b3
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-07-28 23:03:41 +0400

    не меняется кнопка

---
+++ element-search/popups/task.js
@@ -0,0 +1,16 @@
+document.getElementById("modal_main").className = "modal modal_active";
+const close = document.getElementsByClassName('modal__close');
+let arr = Array.from(close);
+const success = document.getElementsByClassName("show-success");
+
+
+arr[0].onclick = function() {
+    document.getElementById("modal_main").className = "modal";
+}
+
+success.onclick = function () {
+    document.getElementsByClassName("show-success").className = "btn_success";
+}
+
+
+
