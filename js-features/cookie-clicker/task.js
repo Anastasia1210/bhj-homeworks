1c9e98a314e3c72bf55d0ae7fb77ce26c2a22914
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-07-30 15:38:57 +0400

    cookie is big and secs are weird

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,15 +1,15 @@
 const image = document.getElementById("cookie");
 const counterID = document.getElementById("clicker__counter");
 let counter = 0;
+let start = 0;
 
     function timeKiller() {
-      let speedCounter = 0;
-      
-      let start = Date();
+               
       image.width = 300;
       counter++;
-      let end = Date();
-      speedCounter = (end - start);
+      let end = (new Date()).getTime();
+      let speedCounter = end - start;
+      start = end;
       counterID.textContent = counter + " Скорость клика: " + speedCounter;
     }
    
