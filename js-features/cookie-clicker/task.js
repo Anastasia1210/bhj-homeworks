ee7c05a4b7639c9f2b3c4814ef75af0dc97172bb
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-01 20:20:12 +0400

    cookie is not changing

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,15 +1,17 @@
 const image = document.getElementById("cookie");
 const counterID = document.getElementById("clicker__counter");
 let counter = 0;
-let start = 0;
+let start = Date.now();
 
     function timeKiller() {
                
       image.width = 300;
       counter++;
-      let end = (new Date()).getTime();
-      let speedCounter = end - start;
+      let end = Date.now();
+      let speedCounter= (end - start) / 1000;
+    
       start = end;
+      
       counterID.textContent = counter + " Скорость клика: " + speedCounter;
     }
    
