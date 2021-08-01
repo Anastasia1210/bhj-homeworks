30eaea6af8e7c760b3345bc3b3c4b79d4340fa4d
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-01 20:42:37 +0400

    done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,14 +2,15 @@ document.getElementById("modal_main").className = "modal modal_active";
 const close = document.getElementsByClassName('modal__close');
 let arr = Array.from(close);
 const success = document.getElementsByClassName("show-success");
+let successArr = Array.from(success)
 
 
 arr[0].onclick = function() {
     document.getElementById("modal_main").className = "modal";
 }
 
-success.onclick = function () {
-    document.getElementsByClassName("show-success").className = "btn_success";
+successArr[0].onclick = function () {
+    document.getElementById("modal_success").className = "modal modal_active";
 }
 
 
