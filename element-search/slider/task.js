acbf1175dcfe51689019919076ba05a99df14989
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-03 18:34:29 +0400

    is not a function

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,7 +1,57 @@
-const slider = document.getElementsByClassName('slider__arrow');
-let arr = Array.from(slider);
+const prev = document.getElementsByClassName('slider__arrow slider__arrow_prev')
+const next = document.getElementsByClassName('slider__arrow slider__arrow_next')
+const slides = document.querySelectorAll('.slider__item')
+const dots = document.querySelectorAll('.slider__dot')
 
-slider.onclick = function(){
-    for (i=0; i<arr.length; i++)
-    arr[i].className = 'slider__item slider__item_active';
+let index = 0;
+
+const activeSlide = n => {
+    console.log(n);
+    for(slide of slides){
+        slide.classList.remove('slider__item_active')
+    }
+    slides[n].classList.add('slider__item_active');
 }
+
+const activeDot = n => {
+    console.log(n);
+    for(dot of dots){
+        dot.classList.remove('_active')
+    }
+    dots[n].classList.add('_active');
+}
+
+const activeItems = ind => {
+    activeDot(ind);
+    activeSlide(ind);
+}
+
+const nextSlide = () => {
+    if(index === slides.length - 1) {
+        index = 0;
+        activeItems(index);
+    } else {
+        index++;
+        activeItems(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index === 0) {
+        index = slides.length - 1;
+        activeItems(index);
+    } else {
+        index--;
+        activeItems(index);
+    }
+}
+
+dots.forEach((item, indexDot) => {
+item.addEventListener('click', () => {
+    index = indexDot;
+    activeItems(index);
+})
+});
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
