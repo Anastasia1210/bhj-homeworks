ff407cd23003facb43cc0f00cd267ca35a259c04
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-03 20:50:05 +0400

    finally...

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,14 +5,22 @@ let start = Date.now();
 
     function timeKiller() {
                
-      image.width = 300;
       counter++;
+      console.log(counter);
+      image.addEventListener('mousedown', () => {
+        image.width = 300;
+      });
+      image.addEventListener('mouseup', () => {
+        image.width = 200;
+      })
+      
       let end = Date.now();
       let speedCounter= (1 / ((end - start) / 1000)).toFixed(3);
     
       start = end;
       
       counterID.textContent = counter + " Скорость клика: " + speedCounter;
+      return counter;
     }
-   
+    
 image.onclick = timeKiller;
