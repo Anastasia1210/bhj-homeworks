91c69d2ed504bb511fbbaef9696429ef2c4f6e63
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-03 20:27:56 +0400

    doesn't work

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,17 @@
-const element = document.getElementsByClassName("menu__link");
-const list = document.getElementsByClassName("menu menu_sub");
-const tag = document.getElementsByTagName('a');
-let arr = Array.from(element);
+const links = document.getElementsByClassName("menu__link");
 
 
-for (let i=0; i<arr.length; i++) {
-    arr[i].addEventListener('click', showSub);
-    //arr[i].addEventListener('click', hideSub, false);
+for (let i=0; i<links.length; i++) {
+    let parent = links[i].closest;
+    links[i].addEventListener('click', () => {
+      if (parent.classList.contains('menu menu_sub') ) {
+          parent.classList.add('menu_active')
+      } else {
+          return false;
+      }
+    });
 }
 
-function showSub(){
-    if(tag.parentElement === list) {
-        list.className = 'menu menu_sub menu_active';
-    }
-    else {
-        return false;
-    }
-}
 
-//function hideSub () {
-  //  if ()
-//}
+
 
