dbb3bab6678c261d9be3b59b8c6bb41366b99f7a
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-08 12:45:45 +0400

    done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,8 +1,16 @@
-const phrases = document.getElementsByClassName('rotator__case');
+const phrases = Array.from(document.getElementsByClassName('rotator__case'));
+let ind = 0;
 
     setInterval(() => {
-      for (item of phrases) {
-        item.classList.toggle('rotator__case_active')}
+      for (el of phrases) {
+        el.classList.remove('rotator__case_active');
+      }
+        phrases[ind].classList.add('rotator__case_active');
+      ind++;
+    if (ind === phrases.length - 1) {
+      ind = 0;
+    }
+    
     }, 1000);
     
 
