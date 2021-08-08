b7b8d8ab9d079df045497e750b4469932c4f4a52
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-08 13:40:20 +0400

    done

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,29 +1,39 @@
 const links = document.querySelectorAll('.font-size');
-const books = document.querySelectorAll('.book__content');
+const fontType = document.getElementById('book');
 
 let arr = Array.from(links);
 
-let index = 0;
-
 const activeLink = n => {
     for (link of links) {
-        link.classList.remove("_active")
+        link.classList.remove("font-size_active")
     }
-    links[n].classList.add("_active");
+    links[n].classList.add("font-size_active");
 }
 
 const fontSize = ind => {
-  if (ind === 0){
-        books[ind].classList.add("book_fs-small")
-    } else if (ind === 2){
-    books[ind].classList.add("book_fs-big");
+    
+  if (links[ind] === links[0]){
+     fontType.classList.remove("book_fs-big");
+     fontType.classList.add("book_fs-small");
+    } else if (links[ind] === links[2]){
+        fontType.classList.remove("book_fs-small");
+        
+        fontType.classList.add("book_fs-big");     
+} else {
+    fontType.classList.remove("book_fs-small");
+    fontType.classList.remove("book_fs-big");
+    
 }
 }
 
 links.forEach((item, indexLink) => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
         index = indexLink;
         activeLink(index);
         fontSize(index);
+
+        
     })
+   
 });
