3ed8ac66a4f02f0158a5a50215691417e49e03c5
Anastasia Gabitova <anastasiagbtv@gmail.com>
2021-08-29 21:53:10 +0400

    not working

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,15 +1,27 @@
 const listOfRadioBtns = document.querySelectorAll('.interests_active');
 
-
-for(i = 0; i < listOfRadioBtns.length; i++) {
-  const btn = listOfRadioBtns[i];
-  const subItems = btn.parentNode.children[i].children[i];
-  console.log(subItems);
-    btn.addEventListener('click', e => { 
-      if(btn.checked) {
-        subItems.checked = true;
+  const btn1 = listOfRadioBtns[0];
+  const items1 = btn1.parentNode.children[0].children[0];
+  
+  for (i = 0; i < btn1.children; i++) {
+  const subItems = btn1.children[i].children[0].children[0];
+    items1.addEventListener('click', e => { 
+      if(items1.checked) {
+        subItems.checked;
       }
-    })
-    
-}
+    })  
+  }  
+
+  const btn2 = listOfRadioBtns[1];
+  const items2 = btn2.parentNode.children[0].children[0];
+  
+  for (i = 0; i < btn2.children; i++) {
+  const subItems = btn2.children[i].children[0].children[0];
+    items2.addEventListener('click', e => { 
+      if(items2.checked) {
+        subItems.checked;
+      }
+    })  
+  } 
+
 
